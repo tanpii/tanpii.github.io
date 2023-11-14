eb44c42bc0522396d10eed401e687fddf65d07f3
tanpii <tporseva@inbox.ru>
2023-11-14 18:02:05 +0300

    catalog filters ad

--- catalog.js
+++ catalog.js
@@ -17,61 +17,91 @@ if (cartJSON) {
 var productDatabase;
 if (localStorage.getItem('productDatabase') == null) {
   productDatabase = [
-    new Product("1", "GINGER CAT", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽299", "/images/cookie_type/ginger_cat.jpg"),
-    new Product("2", "LOVELY PUPPY", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽259", "/images/cookie_type/lovely_puppy.jpg"),
-    new Product("3", "LAZY KITTEN", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽259", "/images/cookie_type/lazy_kitten.jpg"),
-    new Product("4", "CHOCO DOG", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽199", "/images/cookie_type/choco_dog.jpg"),
-    new Product("5", "WHERE TO LOOK?", "Загадочное шоколадное брауни-печенье. Может быть оно смотрит на вас?", "₽300", "/images/cookie_type/where_to_look.jfif"),
-    new Product("6", "SUGAR NIGHTMARE", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/sugar_nightmare.jfif"),
-    new Product("7", "SWEETHEART", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/sweetheart.jfif"),
-    new Product("8", "TEDDY BEAR", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/choco_bear.jfif"),
-    new Product("9", "PINK BANG", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/pink_bang.jpg"),
-    new Product("10", "NUT DREAM", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/nut_dream.jpg"),
-    new Product("11", "GREEN TEA", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/green_tea.jpg"),
-    new Product("12", "OREO BOOM", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/oreo_boom.jpg"),
-    new Product("13", "CARROT LOVERS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/carrot_lover.jpg"),
-    new Product("14", "MARSHMALLOW", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/marshmallow.jpg"),
-    new Product("15", "AMOROUS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/amorous.jpg"),
-    new Product("16", "SOFTNESS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "$6.99", "/images/cookie_type/softness.jpg")
+    new Product("1", "GINGER CAT", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽239", "/images/cookie_type/ginger_cat.jpg", true),
+    new Product("2", "LOVELY PUPPY", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽239", "/images/cookie_type/lovely_puppy.jpg", true),
+    new Product("3", "LAZY KITTEN", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽199", "/images/cookie_type/lazy_kitten.jpg", true),
+    new Product("4", "CHOCO DOG", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽199", "/images/cookie_type/choco_dog.jpg", true),
+    new Product("5", "WHERE TO LOOK?", "Загадочное шоколадное брауни-печенье. Может быть оно смотрит на вас?", "₽359", "/images/cookie_type/where_to_look.jfif"),
+    new Product("6", "SUGAR NIGHTMARE", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽299", "/images/cookie_type/sugar_nightmare.jfif"),
+    new Product("7", "SWEETHEART", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽259", "/images/cookie_type/sweetheart.jfif"),
+    new Product("8", "TEDDY BEAR", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽359", "/images/cookie_type/choco_bear.jfif"),
+    new Product("9", "PINK BANG", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽399", "/images/cookie_type/pink_bang.jpg"),
+    new Product("10", "NUT DREAM", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽299", "/images/cookie_type/nut_dream.jpg"),
+    new Product("11", "GREEN TEA", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽359", "/images/cookie_type/green_tea.jpg"),
+    new Product("12", "OREO BOOM", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽399", "/images/cookie_type/oreo_boom.jpg"),
+    new Product("13", "CARROT LOVERS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽239", "/images/cookie_type/carrot_lover.jpg"),
+    new Product("14", "MARSHMALLOW", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽359", "/images/cookie_type/marshmallow.jpg"),
+    new Product("15", "AMOROUS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽199", "/images/cookie_type/amorous.jpg"),
+    new Product("16", "SOFTNESS", "Ароматное ореховое печенье с хрустящими орехами. Наслаждение каждым кусочком.", "₽299", "/images/cookie_type/softness.jpg")
   ];
   localStorage.setItem('productDatabase', JSON.stringify(productDatabase));
 } else {
   productDatabase = JSON.parse(localStorage.getItem('productDatabase'));
 }
 
-// функция загрузки продуктов на страницу
-function loadProducts() {
-  const catalogContainer = document.querySelector(".shop");
+const specialMenuProducts = productDatabase.filter(product => product.isSpecialMenu === true);
+const favouriteProducts = JSON.parse(localStorage.getItem('favourites'));
 
-  productDatabase.forEach(product => {
+// функция загрузки продуктов на страницу
+function loadProducts(products) {
+  const catalogContainer = document.querySelector(".shop");
+  catalogContainer.innerHTML = '';
+
+  if(!products)
+    return;
+
+  products.forEach(product => {
     const card = document.createElement("div");
     card.classList.add("cookie_type");
 
-    card.innerHTML = `
-      <div class="img-container">
-        <button class="like-button"><i class="heart fa-regular fa-heart"></i></button>
-        <div class="price">${product.price}</div>
-        <img src="${product.imageUrl}" alt="${product.name}">
-      </div>
-      <div class="cookie_type_text">
-        <p class="name"><b>${product.name}</b></p>
-        <p class="info">${product.description}</p>
-      </div>
-      <div class="cart-button" id="${product.id}">
-        <button class="buy-button">КУПИТЬ</button>
-        <div class="addmore">
-          <button class="less-button">-</button>
-          <span class="count">1</span>
-          <button class="more-button">+</button>
+    if(product.isSpecialMenu) {
+      card.innerHTML = `
+        <div class="img-container">
+          <button class="like-button"><i class="heart fa-regular fa-heart"></i></button>
+          <div class="price">${product.price}</div>
+          <div class="season-offer">сезонное предложение</div>
+          <img src="${product.imageUrl}" alt="${product.name}">
         </div>
-      </div>
-    `;
+        <div class="cookie_type_text">
+          <p class="name"><b>${product.name}</b></p>
+          <p class="info">${product.description}</p>
+        </div>
+        <div class="cart-button" id="${product.id}">
+          <button class="buy-button">КУПИТЬ</button>
+          <div class="addmore">
+            <button class="less-button">-</button>
+            <span class="count">1</span>
+            <button class="more-button">+</button>
+          </div>
+        </div>
+      `;
+    } else {
+      card.innerHTML = `
+        <div class="img-container">
+          <button class="like-button"><i class="heart fa-regular fa-heart"></i></button>
+          <div class="price">${product.price}</div>
+          <img src="${product.imageUrl}" alt="${product.name}">
+        </div>
+        <div class="cookie_type_text">
+          <p class="name"><b>${product.name}</b></p>
+          <p class="info">${product.description}</p>
+        </div>
+        <div class="cart-button" id="${product.id}">
+          <button class="buy-button">КУПИТЬ</button>
+          <div class="addmore">
+            <button class="less-button">-</button>
+            <span class="count">1</span>
+            <button class="more-button">+</button>
+          </div>
+        </div>
+      `;
+    }
     catalogContainer.appendChild(card);
   });
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-  loadProducts(); // загружаем продукты на страницу
+  loadProducts(productDatabase); // загружаем продукты на страницу
   
   const cartButtons = document.querySelectorAll(".cart-button");
   cartButtons.forEach(button => {
@@ -137,6 +167,36 @@ document.addEventListener("DOMContentLoaded", function(){
         isTouched = false;
       }
     });
+  });
+
+  var currentProducts = productDatabase;
+
+  const sortSelect = document.getElementById("sortSelect");
+  sortSelect.addEventListener("change", function() {
+    const selectedOption = this.value;
+    if (selectedOption === "default") {
+      loadProducts(currentProducts);
+    } else if (selectedOption === "ascending") {
+      loadProducts([...currentProducts].sort((a, b) => a.price.localeCompare(b.price)));
+    } else if (selectedOption === "descending") {
+      loadProducts([...currentProducts].sort((a, b) => b.price.localeCompare(a.price)));
+    }
+  });
+
+  const typeSelect = document.getElementById("typeSelect");
+  typeSelect.addEventListener("change", function() {
+    sortSelect.value = "default";
+    const selectedOption = this.value;
+    if (selectedOption === "default") {
+      currentProducts = productDatabase;
+      loadProducts(currentProducts);
+    } else if (selectedOption === "favourite") {
+      currentProducts = favouriteProducts;
+      loadProducts(currentProducts);
+    } else if (selectedOption === "season") {
+      currentProducts = specialMenuProducts;
+      loadProducts(currentProducts);
+    }
   });
 });
 
--- product_class.js
+++ product_class.js
@@ -1,11 +1,12 @@
 //класс продукта
 class Product {
-  constructor(id, name, description, price, imageUrl) {
+  constructor(id, name, description, price, imageUrl, isSpecialMenu = false) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.imageUrl = imageUrl;
+    this.isSpecialMenu = isSpecialMenu;
   }
 }
 

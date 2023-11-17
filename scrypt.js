a75d3007c0c68c7a9d9c8a9d4b166e5d6e894c9a
tanpii <tporseva@inbox.ru>
2023-11-17 10:07:40 +0300

    sum

--- scrypt.js
+++ scrypt.js
@@ -1,42 +1,6 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const showLoginFormButton = document.getElementById("showLoginFormButton");
-  const showLoginFormButtonMobile = document.getElementById("showLoginFormButtonMobile");
-  const popupLoginForm = document.getElementById("loginForm");
-  const closeLoginFormButton = document.getElementById("closeLoginFormButton");
-  showLoginFormButton.addEventListener("click", function () {
-    popupLoginForm.style.display = "flex";
-  });
-
-  showLoginFormButtonMobile.addEventListener("click", function () {
-    popupLoginForm.style.display = "flex";
-  });
-
-  closeLoginFormButton.addEventListener("click", function () {
-      popupLoginForm.style.display = "none";
-  });
-
-  document.getElementById('login-button').addEventListener('click', function() {
-      console.log('click');
-      window.location.href = 'catalog.html';
-  });
-
-  const burgerButton = document.querySelector(".header-burger");
-  const mobileMenu = document.querySelector(".mobile-menu");
-
-  burgerButton.addEventListener("click", function(event) {
-    event.stopPropagation(); 
-    if (!mobileMenu.classList.contains("active")) {
-      mobileMenu.classList.add("active");
-      mobileMenu.style.display = "block";
-      burgerButton.style.transform = "rotate(90deg)";
-    } else {
-      mobileMenu.classList.remove("active");
-      mobileMenu.style.display = "none";
-      burgerButton.style.transform = "rotate(0deg)";
-    }
-  });
 
   const products = document.querySelector(".products");
   const product = document.querySelectorAll(".product");
